4bcf31ce4741f21dac199eeca8cd8d63fcc862e7
flwkemd <flwkemd@naver.com>
2017-11-29 01:35:19 +0100

    Concepts of BeanScope & BeanLifeCycle

---
+++ src/com/ex/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,30 @@
+package com.ex.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+	
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("Piniting to the same object: " +result);
+
+		System.out.println("Memory location for theCoach: "+theCoach);
+		System.out.println("Memory location for alphaCoach: "+alphaCoach);
+		
+		// close the context
+		context.close();
+	}
+
+}
